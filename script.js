ab2402f9aa300975afe9d0f7e85f07f8f339ffa0
sivaprabhakar <sivaprabhakaran.94@gmail.com>
2023-09-13 20:20:55 +0530

    food landing

---
+++ script.js
@@ -0,0 +1,5 @@
+// const att = document.createElement("div")
+// att.setAttribute("class","container")
+// att.setAttribute("id","container")
+// att.innerHTML="this is attribute example"
+// document.body.append(att)
